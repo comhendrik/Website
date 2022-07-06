c1c6871cdbb4d1615b6ab4433bba00fdd993efaf
comhendrik <hendrik0807@icloud.com>
2022-07-06 12:49:26 +0200

    english and german support

---
+++ Language/german.py
@@ -0,0 +1,39 @@
+about = {
+    'HELLO': 'Hallo, Ich bin',
+    'INFO': 'Infos über mich:',
+    'FIRSTNAME_INFO': 'Name',
+    'EMAIL_INFO': 'E-Mail',
+    'LASTNAME_INFO': 'Nachname'
+}
+
+article = {
+    'LINK': 'Dieser Artikel ist ebenfalls auf einer anderen Seite verfügbar'
+}
+
+blog = {
+    'HEADER': 'BLOG',
+    'LINK': 'weiterlesen'
+}
+
+cv = {
+    'HEADER': 'Lebenslauf'
+}
+
+portfolio = {
+    'HEADER': 'Portfolio'
+}
+
+index = {
+    'HEADLINE' : 'Hallo, Ich bin',
+    'SUBHEADLINE' : 'Ich programmiere aus Leidenschaft',
+    'ABOUT': 'Über mich',
+    'PORTFOLIOHEADLINE' : 'Mein Portfolio:',
+    'PORTFOLIOSUBHEADLINE' : 'Hier präsentiere ich jedes projekt, welches ich erstellt habe.',
+    'PORTFOLIOBUTTON' : 'Portfolio',
+    'CVHEADLINE' : 'Mein Lebenslauf:',
+    'CVSUBHEADLINE' : 'Dieser enthält alles, was ich in meinem Leben gemacht habe',
+    'CVBUTTON' : 'Lebenslauf',
+    'BLOGHEADLINE' : 'Mein Blog:',
+    'BLOGSUBHEADLINE' : 'Ich teile meine Entdeckungen gerne mit.',
+    'BLOGBUTTON' : 'Blog',
+}
